93d2c2442eb9081243bc9ef288b2588aa264a169
Alex Lohec <alohec@Alexs-MacBook-Pro.local>
2019-09-24 00:21:45 -0700

    Updates

---
+++ precache-manifest.d5139b296bdecb25125423fb7a2c3a2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0bf06288f1b8b7e83f0cdf546e061c6",
+    "url": "/reviews_app/index.html"
+  },
+  {
+    "revision": "505504b72c45162f7d43",
+    "url": "/reviews_app/static/css/2.e71d8db5.chunk.css"
+  },
+  {
+    "revision": "c279740cd81df30c3529",
+    "url": "/reviews_app/static/css/main.d65c7456.chunk.css"
+  },
+  {
+    "revision": "505504b72c45162f7d43",
+    "url": "/reviews_app/static/js/2.5f3cae14.chunk.js"
+  },
+  {
+    "revision": "c279740cd81df30c3529",
+    "url": "/reviews_app/static/js/main.2b2a2213.chunk.js"
+  },
+  {
+    "revision": "8d984dd6b2c548b54ff0",
+    "url": "/reviews_app/static/js/runtime~main.1dd52d87.js"
+  }
+]);
